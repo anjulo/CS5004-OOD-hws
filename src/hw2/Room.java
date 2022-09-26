090e9777f620c89857add01e156f3e40ec83491a
Anjulo Y. Faltamo <anjulo@Anjulos-MacBook-Air.local>
2022-09-26 16:59:56 -0400

    Add Room class Attributes

--- src/hw2/Room.java
+++ src/hw2/Room.java
@@ -5,4 +5,25 @@ package hw2;
  */
 public class Room {
 
+  private int maxOccupancy;
+  private double price;
+  private int numOfGuests;
+
+  /**
+   * The enumerator class for Room type.
+   */
+  enum RoomType {
+    /**
+     * Single room with maximum occupancy of 1.
+     */
+    SINGLE,
+    /**
+     * Double room with maximum occupancy of 2.
+     */
+    DOUBLE,
+    /**
+     * Family room with maximum occupancy of 4.
+     */
+    FAMILY};
+
 }
