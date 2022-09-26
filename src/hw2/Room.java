3d44f53673c8e8e2ded5227da2952b14425e8088
Anjulo Y. Faltamo <anjulo@Anjulos-MacBook-Air.local>
2022-09-26 17:00:20 -0400

    Add Room class methods

--- src/hw2/Room.java
+++ src/hw2/Room.java
@@ -26,4 +26,68 @@ public class Room {
      */
     FAMILY};
 
+  /**
+   * Instantiates a new Room class.
+   *
+   * @param roomType the type of the room
+   * @param price    the price of the room
+   * @throws IllegalArgumentException the illegal argument exception
+   */
+  public Room(RoomType roomType, double price) throws IllegalArgumentException {
+
+    if(price < 0){
+      throw new IllegalArgumentException("Price value can't be negative.");
+    }
+    this.price = price;
+
+    switch (roomType){
+      case SINGLE:
+        this.maxOccupancy = 1;
+        break;
+      case DOUBLE:
+        this.maxOccupancy = 2;
+        break;
+      case FAMILY:
+        this.maxOccupancy = 4;
+        break;
+    }
+
+    this.numOfGuests = 0;
+  }
+
+  /**
+   * Checks if the room is available.
+   *
+   * @return the boolean
+   */
+  public Boolean isAvailable(){
+    if(this.numOfGuests == 0){
+      return true;
+    }
+      return false;
+  }
+
+  /**
+   * Books the room.
+   *
+   * @param numOfGuests the number of guests
+   */
+  public void bookRoom(int numOfGuests){
+    if (this.isAvailable() && 0 < numOfGuests && numOfGuests < this.maxOccupancy){
+      this.numOfGuests = numOfGuests;
+    }
+    // what about cases where the room isn't available?
+    // numOfGuests parameter isn't valid
+
+  }
+
+  /**
+   * Getter for the number of guests in the room.
+   *
+   * @return the int
+   */
+  public int getNumberOfGuests(){
+    return this.numOfGuests;
+  }
+
 }
