47a95a20b984b137361e074116f296f8b1beba19
bereket <iberekety@gmail.com>
2022-09-26 20:54:05 -0400

    Implement attributes and methods for MailItem class

--- src/hw2/MailItem.java
+++ src/hw2/MailItem.java
@@ -1,4 +1,28 @@
 package hw2;
 
 public class MailItem {
+
+  private double width;
+  private double height;
+  private double depth;
+  private Recipient recipient;
+
+  public MailItem(double width, double height, double depth, Recipient recipient)
+                                                throws IllegalArgumentException{
+    if (width < 1 || height < 1 || depth < 1){
+      throw new IllegalArgumentException("Arguments can't be less than 1");
+    }
+    else if (recipient == null){
+      throw new IllegalArgumentException("Recipient can't be null");
+    }
+
+    this.width = width;
+    this.height = height;
+    this.depth = depth;
+    this.recipient = recipient;
+  }
+
+  public Recipient getRecipient(){
+    return this.recipient;
+  }
 }
