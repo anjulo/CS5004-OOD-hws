84222aa6d9c81773c7371bfe1d6dffeca1014a14
bereket <iberekety@gmail.com>
2022-09-27 12:52:43 -0400

    Add enum class for room type

---
+++ src/hw2/RoomType.java
@@ -0,0 +1,19 @@
+package hw2;
+
+/**
+ * The enumerator class for Room type.
+ */
+public enum RoomType {
+  /**
+   * Single room with maximum occupancy of 1.
+   */
+  SINGLE,
+  /**
+   * Double room with maximum occupancy of 2.
+   */
+  DOUBLE,
+  /**
+   * Family room with maximum occupancy of 4.
+   */
+  FAMILY
+}
