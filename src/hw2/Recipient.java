3a18d5a40f57214af63d00efc1c18436cdc954b1
Anjulo Y. Faltamo <anjulo@anjulos-air.myfiosgateway.com>
2022-09-26 20:34:45 -0400

    Implement Recipient's attributes and methods

--- src/hw2/Recipient.java
+++ src/hw2/Recipient.java
@@ -1,4 +1,27 @@
 package hw2;
 
 public class Recipient {
+
+  private String firstName;
+  private String lastName;
+  private String email;
+
+  public Recipient(String firstName, String lastName, String email) throws IllegalArgumentException{
+    if (firstName == null || lastName == null || email ==null){
+      throw new IllegalArgumentException("Arguments can't be null");
+    }
+    else if (firstName == "" || lastName == "" || email == ""){
+      throw new IllegalArgumentException("Arguments can't be empty");
+    }
+
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+  }
+
+  @Override
+  public String toString(){
+    String str = String.format("%s %s Email:%s", firstName, lastName, email);
+    return str;
+  }
 }
