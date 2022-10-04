cca9da75d970c1c7cff2d8341ab60f0ebc2a493a
bereket <iberekety@gmail.com>
2022-10-04 10:19:25 -0400

    Add test classes for each chess piece type

---
+++ test/TestAbstractChessPiece.java
@@ -0,0 +1,41 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestAbstractChessPiece {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @Test
+  public void getRow() {
+  }
+
+  @Test
+  public void getColumn() {
+  }
+
+  @Test
+  public void getColor() {
+  }
+
+  @Test
+  public void canMove() {
+  }
+
+  @Test
+  public void canKill() {
+  }
+
+  @Test
+  public void checkBound() {
+  }
+
+  @Test
+  public void isOnHorOrVerLine() {
+  }
+
+  @Test
+  public void isOnDiagonalLine() {
+  }
+}
---
+++ test/TestPawn.java
@@ -0,0 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestPawn {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @Test
+  public void canMove() {
+  }
+
+  @Test
+  public void canKill() {
+  }
+
+  @Test
+  public void isOnRoyalRow() {
+  }
+}
