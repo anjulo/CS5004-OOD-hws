a53d06d37136fbad0def8515366e65970313e561
bereket <iberekety@gmail.com>
2022-10-05 21:11:44 -0400

    Add javadocs and apply checkstyle.

--- test/TestQueen.java
+++ test/TestQueen.java
@@ -1,64 +1,91 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import chess.Bishop;
+import chess.Color;
+import chess.King;
+import chess.Knight;
+import chess.Pawn;
+import chess.Queen;
+import chess.Rook;
 import org.junit.Before;
 import org.junit.Test;
 
-import chess.*;
-
-import static org.junit.Assert.*;
-
+/**
+ * The test class for the queen class.
+ */
 public class TestQueen {
 
+  /**
+   * The Piece 1.
+   */
   Queen piece1;
+
+  /**
+   * Create the queen objects for test purpose.
+   *
+   * @throws IllegalArgumentException if the position is out of bound.
+   */
   @Before
-  public void setUp() throws Exception {
-    piece1 = new Queen(5,2, Color.BLACK);
+  public void setUp() throws IllegalArgumentException {
+    piece1 = new Queen(5, 2, Color.BLACK);
   }
 
+  /**
+   * Test method for the canMove() method.
+   */
   @Test
-  public void canMove() {
+  public void testCanMove() {
 
-    assertTrue(piece1.canMove(5,0));
-    assertTrue(piece1.canMove(5,1));
-    assertTrue(piece1.canMove(5,3));
-    assertTrue(piece1.canMove(5,4));
+    assertTrue(piece1.canMove(5, 0));
+    assertTrue(piece1.canMove(5, 1));
+    assertTrue(piece1.canMove(5, 3));
+    assertTrue(piece1.canMove(5, 4));
 
-    assertTrue(piece1.canMove(4,1));
-    assertTrue(piece1.canMove(4,2));
-    assertTrue(piece1.canMove(4,3));
+    assertTrue(piece1.canMove(4, 1));
+    assertTrue(piece1.canMove(4, 2));
+    assertTrue(piece1.canMove(4, 3));
 
-    assertTrue(piece1.canMove(6,1));
-    assertTrue(piece1.canMove(6,2));
-    assertTrue(piece1.canMove(6,3));
+    assertTrue(piece1.canMove(6, 1));
+    assertTrue(piece1.canMove(6, 2));
+    assertTrue(piece1.canMove(6, 3));
 
-    assertTrue(piece1.canMove(3,0));
-    assertTrue(piece1.canMove(3,2));
-    assertTrue(piece1.canMove(3,4));
+    assertTrue(piece1.canMove(3, 0));
+    assertTrue(piece1.canMove(3, 2));
+    assertTrue(piece1.canMove(3, 4));
 
-    assertTrue(piece1.canMove(7,0));
-    assertTrue(piece1.canMove(7,2));
-    assertTrue(piece1.canMove(7,4));
+    assertTrue(piece1.canMove(7, 0));
+    assertTrue(piece1.canMove(7, 2));
+    assertTrue(piece1.canMove(7, 4));
 
-    assertFalse(piece1.canMove(5,2));
-    assertFalse(piece1.canMove(3,1));
-    assertFalse(piece1.canMove(3,3));
-    assertFalse(piece1.canMove(7,1));
-    assertFalse(piece1.canMove(7,3));
+    assertFalse(piece1.canMove(5, 2));
+    assertFalse(piece1.canMove(3, 1));
+    assertFalse(piece1.canMove(3, 3));
+    assertFalse(piece1.canMove(7, 1));
+    assertFalse(piece1.canMove(7, 3));
 
-    assertFalse(piece1.canMove(4,0));
-    assertFalse(piece1.canMove(4,4));
-    assertFalse(piece1.canMove(6,0));
-    assertFalse(piece1.canMove(6,4));
+    assertFalse(piece1.canMove(4, 0));
+    assertFalse(piece1.canMove(4, 4));
+    assertFalse(piece1.canMove(6, 0));
+    assertFalse(piece1.canMove(6, 4));
   }
 
+  /**
+   * Test method for exceptions thrown by canMove() method.
+   */
   @Test(expected = IllegalArgumentException.class)
-  public void testCanMoveException(){
-    assertFalse(piece1.canMove(-1,2));
-    assertFalse(piece1.canMove(5,-1));
-    assertFalse(piece1.canMove(8,2));
-    assertFalse(piece1.canMove(5,8));
+  public void testCanMoveException() {
+    assertFalse(piece1.canMove(-1, 2));
+    assertFalse(piece1.canMove(5, -1));
+    assertFalse(piece1.canMove(8, 2));
+    assertFalse(piece1.canMove(5, 8));
   }
 
+  /**
+   * Test method for canKill() method.
+   */
   @Test
-  public void testCanKill(){
+  public void testCanKill() {
 
     assertTrue(piece1.canKill(new Bishop(5, 0, Color.WHITE)));
     assertTrue(piece1.canKill(new Knight(5, 1, Color.WHITE)));
--- test/TestRook.java
+++ test/TestRook.java
@@ -1,45 +1,73 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import chess.Bishop;
+import chess.Color;
+import chess.King;
+import chess.Knight;
+import chess.Pawn;
+import chess.Queen;
+import chess.Rook;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-import chess.*;
-
+/**
+ * The test class for the rook class.
+ */
 public class TestRook {
 
+  /**
+   * The Piece 1.
+   */
   Rook piece1;
+
+  /**
+   * Create the queen objects for test purpose.
+   *
+   * @throws IllegalArgumentException if the position is out of bound.
+   */
   @Before
-  public void setUp() throws Exception {
-    piece1 = new Rook(5,2, Color.WHITE);
+  public void setUp() throws IllegalArgumentException {
+    piece1 = new Rook(5, 2, Color.WHITE);
   }
 
+  /**
+   * Test method for the canMove() method.
+   */
   @Test
   public void testCanMove() {
 
-    assertTrue(piece1.canMove(5,0));
-    assertTrue(piece1.canMove(5,1));
-    assertTrue(piece1.canMove(5,3));
-    assertTrue(piece1.canMove(5,4));
-    assertTrue(piece1.canMove(3,2));
-    assertTrue(piece1.canMove(4,2));
-    assertTrue(piece1.canMove(6,2));
-    assertTrue(piece1.canMove(7,2));
+    assertTrue(piece1.canMove(5, 0));
+    assertTrue(piece1.canMove(5, 1));
+    assertTrue(piece1.canMove(5, 3));
+    assertTrue(piece1.canMove(5, 4));
+    assertTrue(piece1.canMove(3, 2));
+    assertTrue(piece1.canMove(4, 2));
+    assertTrue(piece1.canMove(6, 2));
+    assertTrue(piece1.canMove(7, 2));
 
-    assertFalse(piece1.canMove(4,1));
-    assertFalse(piece1.canMove(4,3));
-    assertFalse(piece1.canMove(6,1));
-    assertFalse(piece1.canMove(6,3));
+    assertFalse(piece1.canMove(4, 1));
+    assertFalse(piece1.canMove(4, 3));
+    assertFalse(piece1.canMove(6, 1));
+    assertFalse(piece1.canMove(6, 3));
   }
 
+  /**
+   * Test method for exceptions thrown by canMove() method.
+   */
   @Test(expected = IllegalArgumentException.class)
-  public void testCanMoveException(){
-    assertFalse(piece1.canMove(-1,2));
-    assertFalse(piece1.canMove(5,-1));
-    assertFalse(piece1.canMove(8,2));
-    assertFalse(piece1.canMove(5,8));
+  public void testCanMoveException() {
+    assertFalse(piece1.canMove(-1, 2));
+    assertFalse(piece1.canMove(5, -1));
+    assertFalse(piece1.canMove(8, 2));
+    assertFalse(piece1.canMove(5, 8));
   }
 
+  /**
+   * Test method for canKill() method.
+   */
   @Test
-  public void testCanKill(){
+  public void testCanKill() {
 
     assertTrue(piece1.canKill(new Bishop(5, 0, Color.BLACK)));
     assertTrue(piece1.canKill(new Knight(5, 1, Color.BLACK)));
@@ -59,6 +87,5 @@ public class TestRook {
     assertFalse(piece1.canKill(new Knight(5, 1, Color.WHITE)));
     assertFalse(piece1.canKill(new Pawn(5, 3, Color.WHITE)));
     assertFalse(piece1.canKill(new Queen(5, 4, Color.WHITE)));
-
   }
 }
