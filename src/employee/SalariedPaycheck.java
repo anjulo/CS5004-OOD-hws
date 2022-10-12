12e02d9ab1365c6a8e89e7d4dd7e595b17288f08
bereket <iberekety@gmail.com>
2022-10-12 15:59:23 -0400

    Implement methods of SalariedPaycheck

--- src/employee/SalariedPaycheck.java
+++ src/employee/SalariedPaycheck.java
@@ -1,4 +1,16 @@
 package employee;
 
-public class SalariedPaycheck {
+public class SalariedPaycheck extends abstractPaycheck implements IPaycheck {
+
+  private int payInterval; //(1, 2 or 4)
+  public SalariedPaycheck(double payRate, int payInterval) throws IllegalArgumentException {
+    super(payRate);
+    this.payInterval = payInterval;
+  }
+
+  public double getTotalPay(){
+    return (payRate/52 * payInterval);
+  }
+
+
 }
