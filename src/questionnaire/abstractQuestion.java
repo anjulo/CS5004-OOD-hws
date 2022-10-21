c628b42beb6a18d8cd92278e937405de1a540799
bereket <iberekety@gmail.com>
2022-10-21 17:19:13 -0400

    Add absstract methods

--- src/questionnaire/abstractQuestion.java
+++ src/questionnaire/abstractQuestion.java
@@ -1,4 +1,33 @@
 package questionnaire;
 
-public abstract class abstractQuestion {
+public abstract class abstractQuestion implements Question{
+
+  private String prompt;
+  private Boolean isRequired;
+  private String answer;
+
+  public abstractQuestion(String prompt, Boolean isRequired){
+    this.prompt = prompt;
+    this.isRequired = isRequired;
+  }
+
+  public  String getPrompt(){
+    return this.prompt;
+  }
+  public Boolean isRequired(){
+    return this.isRequired;
+  }
+
+  public void answer(String answer){
+    this.answer = answer;
+  }
+
+  public  String getAnswer(){
+    return this.answer;
+  }
+
+  public Question Copy(){
+
+  }
+
 }
