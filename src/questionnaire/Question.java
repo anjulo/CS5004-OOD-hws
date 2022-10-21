875014f046095a0c5af844860d1b8f1efe29c53c
bereket <iberekety@gmail.com>
2022-10-21 15:53:51 -0400

    Add abstract method to Question interface

--- src/questionnaire/Question.java
+++ src/questionnaire/Question.java
@@ -1,4 +1,15 @@
 package questionnaire;
 
 public interface Question {
+
+  String getPrompt();
+
+  Boolean isRequired();
+
+  void answer(String response);
+
+  String getAnswer();
+
+  Question Copy();
+
 }
