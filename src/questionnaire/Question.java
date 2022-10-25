cad8401aec9952a314e8d2115b0762ddb5a0f291
bereket <iberekety@gmail.com>
2022-10-25 13:40:52 -0400

    Apply checkstyle

--- src/questionnaire/Question.java
+++ src/questionnaire/Question.java
@@ -1,15 +1,44 @@
 package questionnaire;
 
+/**
+ * The Question interface.
+ */
 public interface Question {
 
+  /**
+   * Gets the question prompt.
+   *
+   * @return the prompt
+   */
   String getPrompt();
 
+  /**
+   * Checks if the question is required.
+   *
+   * @return the boolean
+   */
   Boolean isRequired();
 
+  /**
+   * Sets the answer to the input string.
+   *
+   * @param answer the answer
+   */
   void answer(String answer);
 
+  /**
+   * Gets answer of the question.
+   *
+   * @return the answer
+   */
   String getAnswer();
 
+  /**
+   * implements deep copy of question.
+   *
+   * @return the question
+   * @throws CloneNotSupportedException the clone not supported exception
+   */
   Question copy() throws CloneNotSupportedException;
 
 }
