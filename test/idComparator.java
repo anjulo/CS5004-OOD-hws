d42d6088f3439f8ffdd846c3de2a91d86839e1c6
bereket <iberekety@gmail.com>
2022-10-27 10:56:53 -0400

    Create a comparator for the questionnaire

---
+++ test/idComparator.java
@@ -0,0 +1,10 @@
+import java.util.Comparator;
+
+import questionnaire.Question;
+
+public class idComparator implements Comparator<Question> {
+  public int compare(Question q1, Question q2){
+    return Integer.compare(q1.getPrompt().length(), q2.getPrompt().length());
+    //return q1.getPrompt().compareTo(q2.getPrompt());
+  }
+}
