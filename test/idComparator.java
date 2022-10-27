d756da77da0c274264c83cdfa85644d3939113f2
bereket <iberekety@gmail.com>
2022-10-27 11:53:21 -0400

    Apply checkstyle

--- test/idComparator.java
+++ test/idComparator.java
@@ -6,6 +6,12 @@ import questionnaire.Question;
  * Comparator class for questionnaire.
  */
 public class idComparator implements Comparator<Question> {
+  /**
+   * Returns integer value as a result of comparison.
+   * @param q1 the first object to be compared.
+   * @param q2 the second object to be compared.
+   * @return int
+   */
   public int compare(Question q1, Question q2) {
     return Integer.compare(q1.getPrompt().length(), q2.getPrompt().length());
     //return q1.getPrompt().compareTo(q2.getPrompt());
