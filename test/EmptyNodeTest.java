a9b6557182b5551db49131825b748abea52495a3
bereket <iberekety@gmail.com>
2022-11-04 10:49:15 -0400

    Apply checkstyle for test

--- test/EmptyNodeTest.java
+++ test/EmptyNodeTest.java
@@ -1,35 +1,66 @@
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
+/**
+ * Test for Empty node class.
+ */
 public class EmptyNodeTest {
 
+  /**
+   * Sets up example objects to be used for test.
+   *
+   * @throws Exception the exception
+   */
   @Before
   public void setUp() throws Exception {
   }
 
+  /**
+   * Tests isEmpty().
+   */
   @Test
   public void isEmpty() {
   }
 
+  /**
+   * Tests add().
+   */
   @Test
   public void add() {
   }
 
+  /**
+   * Tests peek().
+   */
   @Test
   public void peek() {
   }
 
+  /**
+   * Tests pop().
+   */
   @Test
   public void pop() {
   }
 
+  /**
+   * Tests toString().
+   */
   @Test
   public void testToString() {
   }
 
+  /**
+   * Tests equals().
+   */
   @Test
   public void testEquals() {
+  }
+
+  /**
+   * Tests hashCode().
+   */
+  @Test
+  public void testHashCode() {
   }
 }
--- test/ListPriorityQueueTest.java
+++ test/ListPriorityQueueTest.java
@@ -1,42 +1,72 @@
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
+/**
+ * Test for priority queue class.
+ */
 public class ListPriorityQueueTest {
 
+  /**
+   * Sets up example objects to be used for test.
+   *
+   * @throws Exception the exception
+   */
   @Before
   public void setUp() throws Exception {
   }
 
+  /**
+   * Tests createEmpty().
+   */
   @Test
   public void createEmpty() {
   }
 
+  /**
+   * Tests isEmpty().
+   */
   @Test
   public void isEmpty() {
   }
 
+  /**
+   * Tests add().
+   */
   @Test
   public void add() {
   }
 
+  /**
+   * Tests peek().
+   */
   @Test
   public void peek() {
   }
 
+  /**
+   * Tests pop().
+   */
   @Test
   public void pop() {
   }
 
+  /**
+   * Tests toString().
+   */
   @Test
   public void testToString() {
   }
 
+  /**
+   * Tests equals().
+   */
   @Test
   public void testEquals() {
   }
 
+  /**
+   * Tests hashCode().
+   */
   @Test
   public void testHashCode() {
   }
