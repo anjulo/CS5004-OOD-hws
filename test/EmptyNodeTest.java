cfd32581049f455f7a3f072d6762f61b2744df24
bereket <iberekety@gmail.com>
2022-11-10 11:00:12 -0500

    Add test

--- test/EmptyNodeTest.java
+++ test/EmptyNodeTest.java
@@ -1,10 +1,23 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import cs5004.collections.EmptyPriorityQueueException;
+import cs5004.collections.ListPriorityQueue;
+import cs5004.collections.PriorityQueue;
 import org.junit.Before;
 import org.junit.Test;
+
 
 /**
  * Test for Empty node class.
  */
 public class EmptyNodeTest {
+
+  PriorityQueue q0;
+  PriorityQueue q1;
+  PriorityQueue q2;
+  PriorityQueue q3;
+  PriorityQueue q01;
 
   /**
    * Sets up example objects to be used for test.
@@ -13,6 +26,10 @@ public class EmptyNodeTest {
    */
   @Before
   public void setUp() throws Exception {
+    q0 = ListPriorityQueue.createEmpty();
+    q01 = ListPriorityQueue.createEmpty();
+
+
   }
 
   /**
@@ -20,6 +37,7 @@ public class EmptyNodeTest {
    */
   @Test
   public void isEmpty() {
+    assertTrue(q0.isEmpty());
   }
 
   /**
@@ -27,27 +45,70 @@ public class EmptyNodeTest {
    */
   @Test
   public void add() {
+    q1 = q0.add(1, "one");
+    assertEquals(q1, new ListPriorityQueue(1, "one", q0));
+    q2 = q0.add(10, "ten");
+    assertEquals(q2, new ListPriorityQueue(10, "ten", q0));
+    q3 = q0.add(5, "five");
+    assertEquals(q3, new ListPriorityQueue(5, "five", q0));
+
+  }
+
+  /**
+   * Test exceptions in add().
+   */
+  @Test
+  public void addExceptions() {
+    int exceptionsCount = 0;
+    try {
+      q1 = q0.add(0, "zero");
+    } catch (IllegalArgumentException e) {
+      exceptionsCount++;
+    }
+    try {
+      q1 = q0.add(11, "eleven");
+    } catch (IllegalArgumentException e) {
+      exceptionsCount++;
+    }
+    assertEquals(2, exceptionsCount);
   }
 
   /**
    * Tests peek().
    */
   @Test
-  public void peek() {
+  public void peek() throws EmptyPriorityQueueException {
+    int exceptionsCount = 0;
+    try {
+      q0.peek();
+    } catch (EmptyPriorityQueueException e) {
+      exceptionsCount++;
+    }
+    try {
+      q01.peek();
+    } catch (EmptyPriorityQueueException e) {
+      exceptionsCount++;
+    }
+    assertEquals(2, exceptionsCount);
   }
 
   /**
    * Tests pop().
    */
   @Test
-  public void pop() {
-  }
-
-  /**
-   * Tests toString().
-   */
-  @Test
-  public void testToString() {
+  public void pop() throws EmptyPriorityQueueException {
+    int exceptionsCount = 0;
+    try {
+      q0.pop();
+    } catch (EmptyPriorityQueueException e) {
+      exceptionsCount++;
+    }
+    try {
+      q01.pop();
+    } catch (EmptyPriorityQueueException e) {
+      exceptionsCount++;
+    }
+    assertEquals(2, exceptionsCount);
   }
 
   /**
--- test/ListPriorityQueueTest.java
+++ test/ListPriorityQueueTest.java
@@ -1,3 +1,10 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import cs5004.collections.EmptyNode;
+import cs5004.collections.EmptyPriorityQueueException;
+import cs5004.collections.ListPriorityQueue;
+import cs5004.collections.PriorityQueue;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -6,13 +13,22 @@ import org.junit.Test;
  */
 public class ListPriorityQueueTest {
 
+  PriorityQueue q0;
+  PriorityQueue q1;
+  PriorityQueue q2;
+  PriorityQueue q3;
+  PriorityQueue q4;
   /**
    * Sets up example objects to be used for test.
    *
    * @throws Exception the exception
    */
+
   @Before
-  public void setUp() throws Exception {
+  public void setUp() {
+    q0 = new EmptyNode();
+    q1 = new ListPriorityQueue(1, "one", q0);
+    q2 = new ListPriorityQueue(2, "two", q1);
   }
 
   /**
@@ -20,6 +36,8 @@ public class ListPriorityQueueTest {
    */
   @Test
   public void createEmpty() {
+    q3 = ListPriorityQueue.createEmpty();
+    assertEquals(q0, q2);
   }
 
   /**
@@ -27,6 +45,8 @@ public class ListPriorityQueueTest {
    */
   @Test
   public void isEmpty() {
+    assertFalse(q1.isEmpty());
+    assertFalse(q2.isEmpty());
   }
 
   /**
@@ -34,27 +54,51 @@ public class ListPriorityQueueTest {
    */
   @Test
   public void add() {
+    q3 = q1.add(3, "three");
+    assertEquals(new ListPriorityQueue(3, "three", q1), q3);
+    q4 = q2.add(10, "ten");
+    assertEquals(new ListPriorityQueue(10, "ten", q2), q4);
+  }
+
+  /**
+   * Test exceptions in add().
+   */
+  @Test
+  public void addExceptions() {
+    int exceptionsCount = 0;
+    try {
+      q3 = q1.add(0, "zero");
+    } catch (IllegalArgumentException e) {
+      exceptionsCount++;
+    }
+    try {
+      q4 = q2.add(11, "eleven");
+    } catch (IllegalArgumentException e) {
+      exceptionsCount++;
+    }
+    assertEquals(2, exceptionsCount);
   }
 
   /**
    * Tests peek().
    */
   @Test
-  public void peek() {
+  public void peek() throws EmptyPriorityQueueException {
+    assertEquals("one", q1.peek());
+    assertEquals("two", q2.peek());
   }
 
   /**
    * Tests pop().
    */
   @Test
-  public void pop() {
-  }
+  public void pop() throws EmptyPriorityQueueException {
 
-  /**
-   * Tests toString().
-   */
-  @Test
-  public void testToString() {
+    q3 = q2.add(3, "three");
+
+    assertEquals(q2, q3.pop());
+    assertEquals(q1, q2.pop());
+    assertEquals(q0, q1.pop());
   }
 
   /**
